1868c612d18925dbdef05135890bf33770b6731a
Mazuh <marcell-mz@hotmail.com>
2016-10-03 01:20:48 -0300

    Add delete and find functions

--- project_1_lua/guests.lua
+++ project_1_lua/guests.lua
@@ -1,7 +1,7 @@
 
 -- Array of guests list and its length
-guests = { }
-guestsLen = 0
+guests = { } -- numeric array [number] { name: string, cpf: string, isRemoved: string }
+guestsLen = 0 -- zero ins't an avaiable index (lua pattern)
 
 -- Create a new guest, add it to list and update the size number.
 -- PARAM
@@ -14,7 +14,7 @@ function addGuest(name, cpf)
     local guest = {
         ['name'] = name,
         ['cpf'] = cpf,
-        ['isVisible'] = true,
+        ['isRemoved'] = nil,
     }
     guestsLen = guestsLen + 1
 
@@ -23,22 +23,46 @@ function addGuest(name, cpf)
     return true
 end
 
-function removeGuest(reason)
-    -- TODO
+-- Remove guest (visibility) from list with a reason (his data is still avaiable by some specific functions)
+-- PARAM
+--  cpf    : string valid personal ID (t: cadastro de pessoa física) formatted as XXX.XXX.XXX-XX
+--  reason : string any text explaining why this guest has been removed
+-- RETURN
+--  true  : boolean if a guest was found and removed
+--  false : boolean if couldn't find this guest or he/she was already been removed
+function removeGuest(cpf, reason)
+    local guest = findGuestByCPF(cpf)
+    
+    if (guest) then
+        if not(guest.isRemoved) then
+            guest.isRemoved = reason
+            return true
+        end
+    end
+
+    return false
 end
 
-function getGuestByCPF(cpf)
-    -- TODO
-end
+-- Find a guest from list, if it is visible or not (equivalent to: 'removed' or not)
+-- PARAM
+--  cpf   : string valid personal ID (t: cadastro de pessoa física) formatted as XXX.XXX.XXX-XX
+-- RETURN
+--  table : element of guests array, if a guest was found
+--  false : boolean if couldn't find this guest 
+function findGuestByCPF(cpf)
+    for i=1, guestsLen do
+        if (guests[i].cpf == cpf) then
+            return guests[i]
+        end
+    end
 
-function getAllGuests()
-    -- TODO
+    return false
 end
 
 -- Print all non-removed guests in format "Person Name (XXX.XXX.XXX-XX)" 
 function printGuests()
     for i=1, guestsLen do
-        if (guests[i].isVisible) then
+        if (not(guests[i].isRemoved)) then
             print(guests[i].name.." ("..guests[i].cpf..")")
         end
     end
@@ -47,12 +71,31 @@ end
 -- Print all removed guests in format "Person Name (XXX.XXX.XXX-XX)" 
 function printRemovedGuests()
     for i=1, guestsLen do
-        if not (guests[i].isVisible) then
-            print(guests[i].name.." ("..guests[i].cpf..")")
+        if (guests[i].isRemoved) then
+            print(guests[i].name.." ("..guests[i].cpf..") - Removed for: "..guests[i].isRemoved)
         end
     end
 end 
 
---addGuest("Um nome", "Um cpf")
---addGuest("Dois nome", "Dois cpf")
---printGuests()
+addGuest("Marcell Guilherme", "000.000.00-11")
+addGuest("Yuri Henrique", "000.000.000-22")
+print("--> Added 2 guests: Marcell and his minion Yuri")
+printGuests()
+
+print()
+
+removeGuest("000.000.000-22", "For being noob")
+print("--> Removed: Yuri")
+printGuests()
+
+print()
+
+addGuest("Yuri Fake", "000.000.000-11")
+addGuest("Italo Vieira", "000.000.000-33")
+print("--> Tried to add: Yuri (faking Marcell's CPF) and Italo (a regular non-nigga)")
+printGuests()
+
+print()
+
+print("--> Printing removed guests and reasons...")
+printRemovedGuests()
