236bbcf9d2c41e505654acfc045c157e52b9f44d
Mazuh <marcell-mz@hotmail.com>
2016-10-03 03:46:22 -0300

    Add persistence lib and new functions

--- project_1_lua/guests.lua
+++ project_1_lua/guests.lua
@@ -1,9 +1,18 @@
 
 -- Implements: https://github.com/BugginhoDeveloper/mini-projeto-1-lua/blob/master/README.md
 
+
+-- dependencies...
+json = require "lib/json"
+
+
 -- Array of guests list and its length
 guests = { } -- numeric array [number] { name: string, cpf: string, isRemoved: string }
 guestsLen = 0 -- zero ins't an avaiable index (lua pattern)
+
+-- local url to persist guests data
+PATH_STORAGE = "data/guests.json"
+
 
 -- Create a new guest, add it to list and update the size number.
 -- PARAM
@@ -81,14 +90,72 @@ function printRemovedGuests()
     end
 end 
 
+-- Decode guests list JSON stored in a local (existent) file, bringing it to var guests.
+function loadDataFromFile()
+    file = io.open(PATH_STORAGE, "r")
+
+    io.input(file)
+
+    contentFound = io.read()
+    
+    if (contentFound == "" or contentFound == nil) then
+        io.close(file)
+        createOrClearFile()
+        return false
+    end
+
+    guests = json.decode(contentFound)
+    io.close(file)
+    return true
+end
+
+-- Encode guests list into a JSON and stores it in a local file.
+function saveDataInFile()
+    file = io.open(PATH_STORAGE, "w")
+
+    io.output(file)
+
+    if (io.write(json.encode(guests))) then
+        io.close(file)
+        return true
+    end
+    
+    io.close(file)
+    return false
+end
+
+-- Try to create an empty file or clear all data in the existent path 
+function createOrClearFile()
+    file = io.open(PATH_STORAGE, "w")
+
+    io.output(file)
+
+    if (io.write()) then
+        io.close(file)
+        return true
+    end
+    
+    io.close(file)
+    return false
+end
 
 
 
 -- TESTS
 
+print("Trying to read "..PATH_STORAGE.." (if it doesn't work or it's taking too long, manually create an empty file in this path and/or check scripts permission level)...")
+if (loadDataFromFile()) then
+    print("Previous data found in our files and loaded.")
+    printGuests()
+else
+    print("There was no data to load.")
+end
+
+print()
+
 addGuest("Marcell Guilherme", "000.000.000-11")
 addGuest("Yuri Henrique", "000.000.000-22")
-print("--> Added 2 guests: Marcell and his minion Yuri")
+print("--> Tried to add 2 guests: Marcell and his minion Yuri")
 printGuests()
 
 print()
@@ -101,7 +168,7 @@ print()
 
 addGuest("Yuri Fake", "000.000.000-11") -- you shall not pass!
 addGuest("Italo Vieira", "000.000.000-33")
-print("--> Tried to add: Yuri (faking Marcell's CPF) and Italo (a regular non-nigga)")
+print("--> Tried to add 2 guests: Yuri (faking Marcell's CPF) and Italo (a regular non-nigga)")
 printGuests()
 
 print()
@@ -111,4 +178,9 @@ printRemovedGuests()
 
 print()
 
-print("This ends our little journey.")
+print("This ends our little journey. Trying to save...")
+saveDataInFile()
+
+print()
+
+print("Saved! See ya.")
