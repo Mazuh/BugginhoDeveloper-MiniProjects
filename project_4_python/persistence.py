57d8575b6f9ea6dca4e15170836cbdc7fe784c43
Marcell Guilherme <Mazuh@users.noreply.github.com>
2016-12-20 22:54:38 -0300

    Create persistence.py

---
+++ project_4_python/persistence.py
@@ -0,0 +1,16 @@
+
+class Persistence(object):
+    """ Data manager of bank accounts.
+    Can also persist memory instances into persistent local storage."""
+
+    users = []
+
+    def save(self):
+        """ TODO
+        """
+        pass
+
+    def load(self):
+        """ TODO
+        """
+        pass
