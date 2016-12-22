b627dd490125c6cd64a3295f3540aa4020fa134b
Marcell Guilherme <Mazuh@users.noreply.github.com>
2016-12-22 02:17:22 -0300

    Clear main

--- project_4_python/main.py
+++ project_4_python/main.py
@@ -2,25 +2,4 @@
 # pylint: disable=C0325
 
 """ Main script to simulate some kind of ATM (Automatic Teller Machine). """
-
-from persistence import Persistence
-from user import User
-
-data = Persistence()
-
-# main user
-user = User('agency', 'account', 'pass')
-user.hash_password()
-print(user.password)
-
-# other users
-friend1 = User('agency', 'account1', 'pass1')
-friend1.hash_password()
-
-friend2 = User('agency', 'account2', 'pass2')
-friend2.hash_password()
-
-# assembling all users for this test
-data.users.append(user)
-data.users.append(friend1)
-data.users.append(friend2)
+# TODO
