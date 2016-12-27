be10e8955347aacb3291084267def9c34b4ce772
Yuri Sales <yuri.sales@protonmail.com>
2016-12-27 16:26:14 -0300

    Traduz português pra inglês

--- project_4_python/main.py
+++ project_4_python/main.py
@@ -12,152 +12,152 @@ from atm import Persistence
 d_manager = Persistence()
 logged_user = None
 
-print('CAIXA ELETRÔNICO')
+print('AUTOMATIC TELLER MACHINE')
 print('Hi, hi, Puffy AmiYumi!!!')
 
 print('')
 
-print('Usuários built-in...')
-print('-> Agência A1:')
-print('Conta: 00000-0 | Senha: pass0')
-print('Conta: 11111-1 | Senha: pass1')
-print('-> Agência A2')
-print('Conta: 22222-2 | Senha: pass2')
-print('Conta: 44444-4 | Senha: pass4')
-print('-> Agência A3')
-print('Conta: 33333-3 | Senha: pass3')
+print('Users built-in...')
+print('-> Agency A1:')
+print('Account: 00000-0 | Password: pass0')
+print('Account: 11111-1 | Password: pass1')
+print('-> Agency A2')
+print('Account: 22222-2 | Password: pass2')
+print('Account: 44444-4 | Password: pass4')
+print('-> Agency A3')
+print('Account: 33333-3 | Password: pass3')
 
 print('')
 
-print('Acesse agora.')
+print('Access now.')
 for i in range(0, 3):
     print('')
-    agency = input('Agência: ')
-    account = input('Conta: ')
-    password = getpass('Senha: ')
+    agency = input('Agency: ')
+    account = input('Account: ')
+    password = getpass('Password: ')
 
     trying_user = d_manager.find_user(agency=agency, account=account)
 
     if trying_user is not None:
         if trying_user.log_in(password):
             logged_user = trying_user
-            print('Usuário autorizado.')
+            print('Authorized user.')
             break
         else:
-            print('Senha incorreta.')
+            print('Wrong password.')
     else:
-        print('Usuário não encontrado.')
+        print('User not found.')
 
 if logged_user is None:
-    print('\nTentativas esgotadas. Adeus!')
+    print('\nAttempts exhausted! Goodbye!')
     exit(666)
 else:
-    print('\nBem-vindo...')
+    print('\nWelcome...')
 
 while True:
     print('''
-SELECIONE UMA OPERAÇÃO
-1 - Saldo       4 - Saque
-2 - Extrato     5 - Transferência
-3 - Depósito    6 - Sair
+Choose an option
+1 - Balance       4 - Withdraw
+2 - Extract       5 - Transfer
+3 - Deposit      6 - Exit
     ''')
     op = input(': ')
     print('')
     #print(150 * '\n') # to clear the terminal
 
     if op[0] == '1': # balance
-        print('SALDO:\nR$' + str(logged_user.get_balance()))
+        print('BALANCE:\n$' + str(logged_user.get_balance()))
 
     elif op[0] == '2': # history
-        print('EXTRATO:')
+        print('EXTRACT:')
         for register in logged_user.get_history():
             print(register[0])
 
     elif op[0] == '3': # deposit
-        print('DEPÓSITO.')
-        amount = float(input('Quantia: R$').replace(',', '.'))
+        print('DEPOSIT.')
+        amount = float(input('Amount: $').replace(',', '.'))
 
-        to_another_user = input('Na própria conta? (y/n): ')[0]
+        to_another_user = input('In your own account? (y/n): ')[0]
         to_another_user = not((to_another_user == 'y') or (to_another_user == 'Y'))
 
         if to_another_user:
-            print('Dados do outro usuário...')
-            another_user_agency = input('Agência: ')
-            another_user_account = input('Conta: ')
+            print('Other user data...')
+            another_user_agency = input('Agency: ')
+            another_user_account = input('Account: ')
             another_user = d_manager.find_user(another_user_agency, another_user_account)
             if another_user is not None:
                 logged_user.deposit(amount, another_user)
-                print('Depósito realizado na conta fornecida acima.')
+                print('Deposit successfully in the account provided above')
             else:
-                print('Usuário não encontrado.')
+                print('User not found.')
         else:
             logged_user.deposit(amount)
-            print('Depósito realizado em sua conta.')
+            print('Deposit successfully in your account.')
 
 
     elif op[0] == '4': # withdraw
-        print('SAQUE.')
-        print('OBS: disponíveis apenas notas de R$100, R$50 e R$20.')
-        print('OBS2: saque máximo de R$1000,00 por vez.')
-        amount = float(input('Quantia: R$').replace(',', '.'))
+        print('WITHDRAW.')
+        print('OBS: Only $100, $50 and $20 bills are available.')
+        print('OBS2: The maximum value you can withdraw is $1000,00.')
+        amount = float(input('Amount: $').replace(',', '.'))
 
-        print('Tentando obter opções de notas para saque...')
+        print('Trying to get bills options to withdraw.')
         options = logged_user.options_to_withdraw(amount)
         if options is None:
-            print('Esse saque não pôde se realizado.')
-            print('Verifique seu saldo e a as formas de saque disponíveis.')
+            print("This withdraw couldn't be held.")
+            print('Verify you balance and the withdraw ways available.')
         else:
             for i in range(0, len(options)):
                 option = options[i]
                 qtt_100s, qtt_50s, qtt_20s = (option[0], option[1], option[2])
-                print('Opção ' + str(i+1) + ':')
+                print('Option ' + str(i+1) + ':')
                 if qtt_100s > 0:
-                    print('\t' + str(qtt_100s) + 'x R$100')
+                    print('\t' + str(qtt_100s) + 'x $100')
                 if qtt_50s > 0:
-                    print('\t' + str(qtt_50s) + 'x R$50')
+                    print('\t' + str(qtt_50s) + 'x $50')
                 if qtt_20s > 0:
-                    print('\t' + str(qtt_20s) + 'x R$20')
+                    print('\t' + str(qtt_20s) + 'x $20')
                 i += 1
 
-            option = options[int(input('Sua opção: ')) - 1]
-            print('Tentando sacar...')
+            option = options[int(input('Your option: ')) - 1]
+            print('Trying to withdraw...')
             qtt_100s, qtt_50s, qtt_20s = (option[0], option[1], option[2])
 
             if logged_user.withdraw_cash(qtt_100s, qtt_50s, qtt_20s):
-                print('Fornecidos {}x R$100, {}x R$50 e {}x R$20.'
+                print('Provided {}x $100, {}x $50 e {}x $20.'
                       .format(qtt_100s, qtt_50s, qtt_20s))
-                print('Saque efetuado com sucesso.')
+                print('Withdraw successfully.')
             else:
-                print('Ocorreu algum erro e o saque não foi efetuado.')
+                print("An error occurred and the withdraw couldn't be held.")
 
     elif op[0] == '5': # transfer
-        print('TRANSFERÊNCIA.')
-        print('Dados do outro usuário...')
-        another_user_agency = input('Agência: ')
-        another_user_account = input('Conta: ')
+        print('TRANSFER.')
+        print('Other user data...')
+        another_user_agency = input('Agency: ')
+        another_user_account = input('Account: ')
         another_user = d_manager.find_user(another_user_agency, another_user_account)
 
         if another_user is not None:
-            amount = float(input('Quantia: R$').replace(',', '.'))
+            amount = float(input('Amount: $').replace(',', '.'))
 
             if logged_user.transfer_to(amount, another_user):
-                print('Transferência realizada com sucesso.')
+                print('Transfer successfully.')
             else:
-                print('Falha ao tentar transferir. Saldo insuficiente?')
+                print('Transfer failed. Isufficient balance?')
         else:
-            print('Usuário não encontrado.')
+            print('User not found.')
 
     elif op[0] == '6': # logout
-        print('SAIR')
+        print('EXIT')
         logged_user.log_out()
-        print('Sessão finalizada com sucesso. Saindo...')
-        print('Tchau!')
+        print('Section closed. Exiting...')
+        print('Bye Bye!')
         exit(0)
 
     else:
-        print('Opção inválida.')
+        print('Invalid option.')
 
     print('')
-    input('Digite Enter para salvar as alterações e voltar...')
+    input('Press Enter to save the changes and go back...')
     d_manager.update_users()
-    print('Pronto! Retornando agora.')
+    print('Ok! Returning now.')
